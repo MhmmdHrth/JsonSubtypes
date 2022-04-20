e16d6ab37b83715763da4aa5dc0d8f7e2d6261b5
Mhmmdhrth <harith781@gmail.com>
2022-04-20 16:58:37 +0800

    add validation for jsonsubtypes

---
+++ JsonSubTypesAPI/Controllers/PersonsController.cs
@@ -0,0 +1,33 @@
+﻿using JsonSubTypesTests;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace JsonSubTypesAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class PersonsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetJsonSubTypes()
+        {
+            string json = "[{\"Department\":\"Department1\",\"JobTitle\":\"JobTitle1\",\"FirstName\":\"FirstName1\",\"LastName\":\"LastName1\"}," +
+                "{\"Department\":\"Department1\",\"JobTitle\":\"JobTitle1\",\"FirstName\":\"FirstName1\",\"LastName\":\"LastName1\"}," +
+                "{\"Skill\":\"\",\"FirstName\":\"\",\"LastName\":\"LastName1\"}]";
+
+            var persons = JsonConvert.DeserializeObject<IReadOnlyCollection<Person>>(json);
+            var artist = persons.Last() as Artist;
+            var context = new ValidationContext(artist);
+            var results = new List<ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(artist, context, results, true);
+            if (isValid)
+            {
+                return Ok("Success");
+            }
+
+            return BadRequest(new { result = results });
+        }
+    }
+}
---
+++ JsonSubTypesAPI/Person.cs
@@ -0,0 +1,28 @@
+﻿using JsonSubTypes;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace JsonSubTypesTests
+{
+    [JsonConverter(typeof(JsonSubtypes))]
+    [JsonSubtypes.KnownSubTypeWithProperty(typeof(Employee), nameof(Employee.JobTitle))]
+    [JsonSubtypes.KnownSubTypeWithProperty(typeof(Artist), nameof(Artist.Skill))]
+    public class Person
+    {
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Employee : Person
+    {
+        public string Department { get; set; }
+        public string JobTitle { get; set; }
+    }
+
+    public class Artist : Person
+    {
+        [MinLength(4)]
+        public string Skill { get; set; }
+    }
+}
